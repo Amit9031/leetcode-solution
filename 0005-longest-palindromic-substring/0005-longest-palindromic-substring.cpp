2bf02f876e93e118e8d781dec22a8847c3281fcc
Amit Ranjan <148646646+Amit9031@users.noreply.github.com>
2026-08-15 16:30:47 +0530

    Time: 71 ms (37.87%), Space: 164.7 MB (17.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
 
-    int solve(string s, int l, int r){
-        int count=0;
-       
-            while(l>=0 && r<s.size()){
-                if(s[l]==s[r]){
-                    count+=2;
-                    l--;
-                    r++;
-                }
-                else {
-                    break;
-                }
-            }
-        
-        return count;
-
-
+int solve(string s ,int l,int r){
+    int count=0;
+    while(l>=0 && r<s.size()){
+        if(s[l]==s[r]){
+        count+=2;
+        l--;
+        r++;
     }
+
+    else{
+        break;
+    }}
+    return count;
+
+
+}
     string longestPalindrome(string s) {
-        int ans=0;
-        int maxi=0;
-        int start=0;
+        int start=0, maxi=0;
+   
         for(int i=0;i<s.size();i++){
-            ans=1+solve(s, i-1, i+1);
+            int ans=1+solve(s, i-1, i+1);
             if(ans>maxi){
                 maxi=ans;
                 start=i-ans/2;
             }
-                
-            }
+        }
 
-
-            for(int i=0;i<s.size();i++){
-            ans=solve(s, i-1, i);
+for(int i=0;i<s.size();i++){
+            int ans=solve(s, i-1, i);
             if(ans>maxi){
                 maxi=ans;
                 start=i-ans/2;
             }
-                
-            }
+        }
         return s.substr(start, maxi);
+
     }
 };
