21ba3c4170d9739f4262c6e32b43e0b89a060f5b
Amit Ranjan <148646646+Amit9031@users.noreply.github.com>
2026-08-20 01:03:56 +0530

    Time: 7 ms (61.42%), Space: 11.4 MB (83.34%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       vector<int>hash(256,0);
-       int n=s.size(),m=t.size(),l=0,r=0,startind=0,minlen=INT_MAX,cnt=0;;
-       for(auto i: t){
-        hash[i]++;
-       }
-       while(r<s.size()){
-        if(hash[s[r]]>0)
-        cnt++;
-        hash[s[r]]--;
-        while(cnt==m){
-            if(r-l+1<minlen){
-                minlen=r-l+1;
-                startind=l;
-            }
-            hash[s[l]]++;
-            if(hash[s[l]]>0)cnt--;
-            l++;
+        int l=0,r=0;
+        int minlen=INT_MAX;
+        unordered_map<char, int>mp;
+        int start=0,count=0;
+        for(auto it: t){
+            mp[it]++;
         }
-        r++;
-       }
-       return minlen==INT_MAX ?"":s.substr(startind,minlen);
+        while(r<s.size()){
+            if(mp[s[r]]>0){
+                count++;
+            }
+            mp[s[r]]--;
+            while(count==t.size()){
+                if(r-l+1<minlen){
+                    start=l;
+                    minlen=r-l+1;
+
+                }
+                mp[s[l]]++;
+                if(mp[s[l]]>0)count--;
+                l++;
+            }
+            r++;
+            
+        }
+        return minlen==INT_MAX?"":s.substr(start, minlen);
+
     }
 };
